6004daf5e1960fb22a00cf1f521810dd1d6edf8a
William Schultz <will627@comcast.net>
2022-04-12 14:11:54 -0400

    More skeleton work on reducing clauses

--- sat.cpp
+++ sat.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
 
 /**
  * A literal is a boolean variable that may be either negated or non-negated.
@@ -38,6 +41,10 @@ class Literal{
             auto prefix = _neg ? "~" : "";
             return prefix + _name;
         }
+
+        std::string getVarName(){
+            return _name;
+        }
 };
 
 
@@ -62,6 +69,24 @@ class Clause{
             // Remove the extra disjunction symbol.
             return outStr.substr(0,outStr.length()-3);
         }
+
+        /**
+         * Return the set of variables that appear in this clause.
+         */
+        std::vector<Literal> getLiterals(){
+            return _elems;
+        }
+
+        /**
+         * Return the set of variables that appear in this clause.
+         */
+        std::set<std::string> getVariableSet(){
+            std::set<std::string> outSet;
+            for(auto e : _elems){
+                outSet.insert(e.getVarName());
+            }
+            return outSet;
+        }
 };
 
 /**
@@ -71,6 +96,8 @@ class CNF{
     private:
         std::vector<Clause> _clauses;
     public:
+        CNF(){}
+
         CNF(std::vector<Clause> clauses){
             _clauses = clauses;
         }
@@ -84,6 +111,48 @@ class CNF{
             // Remove the extra conjunction symbol.
             return outStr.substr(0,outStr.length()-2);            
         }
+
+        // Assign values to the specified variables and return a simplified CNF formula.
+        // Assignment is given as a map from variable names to boolean values.
+        CNF assign(std::map<std::string, bool> assgmt){
+            // Record the set of variables that have been assigned values.
+            std::set<std::string> assignedVars;
+            for(auto it = assgmt.begin(); it != assgmt.end(); it++){
+                assignedVars.insert(it->first);
+            }
+
+            std::vector<Clause> outClauses;
+            for(auto c : _clauses){
+                // auto inter = std::set_intersection(assignedVars.begin(), assignedVars.end(), c.getVariables().begin(), c.getVariables().end())
+                // std::set<std::string> assignedVars;
+
+                // For each literal in this clause, check if it is assigned a value.
+                for(auto lit : c.getLiterals()){
+                    if(assignedVars.find(lit.getVarName()) != assignedVars.end()){
+                        // TODO. Assign variable and reduce clause.
+                    }
+
+                }
+            }
+
+            return CNF();
+        }
+
+};
+
+/**
+ * Satisfiability solver.
+ */
+class Solver{
+    Solver(){
+
+    }
+
+    bool isSat(CNF f){
+        // TODO.
+        return false;
+    }
+
 };
 
 
