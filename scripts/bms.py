de016dc9e2119995fd122106ec857e5b86a0d053
William Schultz <will627@comcast.net>
2022-04-29 15:03:43 -0400

    save plotting

--- scripts/bms.py
+++ scripts/bms.py
@@ -3,6 +3,8 @@ from pysat.formula import CNF
 import time
 import subprocess
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
 bms = [
     "benchmarks/cnf_samples/aim-50-1_6-yes1-4.cnf",
@@ -25,7 +27,7 @@ bms = [
     "benchmarks/flat75-180/flat75-3.cnf",
     "benchmarks/flat100-239/flat100-1.cnf",
     # Pigeonhole.
-    "benchmarks/pigeon-hole/hole6.cnf",
+    # "benchmarks/pigeon-hole/hole6.cnf",
     # "benchmarks/pigeon-hole/hole7.cnf",
     # "benchmarks/pigeon-hole/hole8.cnf"
 ]
@@ -58,7 +60,7 @@ def run_mysat(bms_to_run):
         is_sat = "SAT" if row['is_sat'] else "UNSAT"
         print(f"{bm}: {is_sat}, Solved in {row['duration_ms']}ms")
         csvfile.close()
-        results.append({"bm": bm, "duration_ms": row['duration_ms'], "is_sat": is_sat})
+        results.append({"bm": bm, "duration_ms": float(row['duration_ms']), "is_sat": is_sat})
     return results
 
 def save_results(results, outfilename):
@@ -71,10 +73,23 @@ def save_results(results, outfilename):
 
 # Run MiniSAT on benchmarks.
 print("# Running MiniSAT22")
-results = run_minisat22(bms)
-save_results(results, "results/minisat22_results.csv")
+minisat_results = run_minisat22(bms)
+save_results(minisat_results, "results/minisat22_results.csv")
 
 # Run mysat on benchmarks.
 print("# Running mysat")
-results = run_mysat(bms)
-save_results(results, "results/mysat_results.csv")
+mysat_results = run_mysat(bms)
+save_results(mysat_results, "results/mysat_results.csv")
+
+
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+mysat_durations = [r["duration_ms"] for r in mysat_results]
+minisat_durations = [r["duration_ms"] for r in minisat_results]
+results = [mysat_durations, minisat_durations]
+print(results)
+ax.plot(bms,results[0],color="blue")
+ax.plot(bms,results[1],color="orange")
+# plt.show()
+plt.savefig("results/compare")
